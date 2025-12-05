3618cc50daa5592c77f0c6e9b6ee37260fdbf1a7
Michal Drbohlav <m.drbohlav1@gmail.com>
2025-12-05 09:17:12 +0100

    fix: set proper buffered state prop type

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -16,7 +16,7 @@ export interface HTMLMediaProps {
 }
 
 export interface HTMLMediaState {
-  buffered: any[];
+  buffered: { start: number; end: number }[];
   duration: number;
   paused: boolean;
   muted: boolean;
